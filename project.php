afe657d6bd1659055996c4b5ba9c2ab269fa9ea3
Hasiisewwandi <67278023+Hasiisewwandi@users.noreply.github.com>
2020-08-24 20:02:19 +0530

    Add files via upload

--- project.php
+++ project.php
@@ -1,102 +1,96 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="hotel";
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-if($conn==True)
+
+  $servername="localhost";
+  $username="root";
+  $password="";
+  $db="booking";
+
+$conn=mysqli_connect($servername,$username,$password,$db);
+if($conn==True )
 {
+    
+    if(isset($_GET['Username'])){
+    $name=$_GET['Username'];
+
+  }else{
+    $name="Name not set in GET Method";
+  }
+
+  if(isset($_GET['Contactnumber'])){
+    $Contactnumber=$_GET['Contactnumber'];
+
+  }else{
+    $Contactnumber="Name not set in GET Method";
+  }
+
+  if(isset($_GET['City'])){
+    $City=$_GET['City'];
+
+  }else{
+    $City="City not set in GET Method";
+  }
+
+  if(isset($_GET['side'])){
+    $Roomside=$_GET['side'];
+
+  }else{
+    $Roomside="Side not set in GET Method";
+  }
+
+   if(isset($_GET['Room'])){
+    $Roomtype=$_GET['Room'];
+
+  }else{
+    $Roomtype="Room not set in GET Method";
+  }
+
+   if(isset($_GET['Number'])){
+    $num=$_GET['Number'];
+
+  }else{
+    $num="Number not set in GET Method";
+  }
+
+   if(isset($_GET['guest'])){
+    $Guest=$_GET['guest'];
+
+  }else{
+    $Guest="Guest not set in GET Method";
+  }
+
+   if(isset($_GET['need'])){
+    $Need=$_GET['need'];
+
+  }else{
+    $Need="Need not set in GET Method";
+  }
+   if(isset($_GET['date'])){
+    $Adate=$_GET['date'];
+
+  }else{
+    $Adate="Arrival date is not set in GET Method";
+  }
+   if(isset($_GET['dat'])){
+    $Ddate=$_GET['dat'];
+
+  }else{
+    $Ddate="Departhure date is not set in GET Method";
+  }
+
+    $sql="INSERT INTO rooms(Username,Contactnumber,City,Roomside,Roomtype,Roomnumber,Numberofguests,Whatneed,Arrivaldate,Departuredate)
+    VALUES('$name','$Contactnumber','$City','$Roomside','$Roomtype',$num','$Guest','$Need','$Adate','$Ddate')";
+    if(mysqli_query($conn,$sql)==True)
+    {
+      echo'data inserted';
+
+    } 
+    else{
+      echo'data not inserted';
+    }
 
 
-
-if(isset($_GET['fname']))
-	{$fname=$_GET['fname'];}
+}
 else{
-	$fname="Name not set in get method";
-}
-
-if(isset($_GET['uname']))
-	{$uname=$_GET['uname'];}
-
-else{
-	$uname="Name not set in get method";
-}
-
-
-if(isset($_GET['cnum']))
-	{$cnum=$_GET['cnum'];}
-
-else{
-	$cnum="Name not set in get method";
-}
-
-if(isset($_GET['city']))
-	{$city=$_GET['city'];}
-
-else{
-	$city="Name not set in get method";
-}
-
-if(isset($_GET['guest']))
-	{$guest=$_GET['guest'];}
-
-else{
-	$guest="Name not set in get method";
-}
-
-if(isset($_GET['pickd']))
-	{$pickday=$_GET['pickd'];}
-
-else{
-	$pickday="Name not set in get method";
-}
-
-if(isset($_GET['nuday']))
-	{$numday=$_GET['nuday'];}
-
-else{
-	$numday="Name not set in get method";
-}
-
-if(isset($_GET['dia']))
-	{$dia=$_GET['dia'];}
-
-else{
-	$dia="Name not set in get method";
-}
-
-if(isset($_GET['gol']))
-	{$gol=$_GET['gol'];}
-
-else{
-	$gol="Name not set in get method";
-}
-
-if(isset($_GET['sil']))
-	{$sil=$_GET['sil'];}
-
-else{
-	$sil="Name not set in get method";
-}
-
-
-$sql="INSERT INTO hall_res(uname,cnum,city,bdate,nguest,days,fname,dia,gol,sil) 
-VALUES('$uname','$cnum','$city','$pickday','$guest','$numday','$fname','$dia','$gol','$sil')";
-
-
-if(mysqli_query($conn,$sql)==True)
-{
-	echo 'data inserted';
-}
-else
-{
-	echo 'data not inserted';
-}
-
-}
-else
-{
-	echo "connection error";
-
-}
-
+  echo"Connection failure";
+    } 
+  
