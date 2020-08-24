75fb103cbdeb9ad397602ea5def8254553803d7c
Prajish7 <67283768+Prajish7@users.noreply.github.com>
2020-08-24 19:02:56 +0530

    Add files via upload

--- project.php
+++ project.php
@@ -1,32 +1,102 @@
 <?php
-
 $servername="localhost";
-$username="members";
-$password="password"
-$dbname="test";
+$username="root";
+$password="";
+$dbname="hotel";
 $conn=mysqli_connect($servername,$username,$password,$dbname);
-if(!$conn){
+if($conn==True)
+{
 
-	die("connectionfailed".mysql_connect_error());
+
+
+if(isset($_GET['fname']))
+	{$fname=$_GET['fname'];}
+else{
+	$fname="Name not set in get method";
+}
+
+if(isset($_GET['uname']))
+	{$uname=$_GET['uname'];}
+
+else{
+	$uname="Name not set in get method";
+}
+
+
+if(isset($_GET['cnum']))
+	{$cnum=$_GET['cnum'];}
+
+else{
+	$cnum="Name not set in get method";
+}
+
+if(isset($_GET['city']))
+	{$city=$_GET['city'];}
+
+else{
+	$city="Name not set in get method";
+}
+
+if(isset($_GET['guest']))
+	{$guest=$_GET['guest'];}
+
+else{
+	$guest="Name not set in get method";
+}
+
+if(isset($_GET['pickd']))
+	{$pickday=$_GET['pickd'];}
+
+else{
+	$pickday="Name not set in get method";
+}
+
+if(isset($_GET['nuday']))
+	{$numday=$_GET['nuday'];}
+
+else{
+	$numday="Name not set in get method";
+}
+
+if(isset($_GET['dia']))
+	{$dia=$_GET['dia'];}
+
+else{
+	$dia="Name not set in get method";
+}
+
+if(isset($_GET['gol']))
+	{$gol=$_GET['gol'];}
+
+else{
+	$gol="Name not set in get method";
+}
+
+if(isset($_GET['sil']))
+	{$sil=$_GET['sil'];}
+
+else{
+	$sil="Name not set in get method";
+}
+
+
+$sql="INSERT INTO hall_res(uname,cnum,city,bdate,nguest,days,fname,dia,gol,sil) 
+VALUES('$uname','$cnum','$city','$pickday','$guest','$numday','$fname','$dia','$gol','$sil')";
+
+
+if(mysqli_query($conn,$sql)==True)
+{
+	echo 'data inserted';
+}
+else
+{
+	echo 'data not inserted';
+}
 
 }
 else
 {
-	$Username=$_POST['Username'];
-	$UserID=$_POST['UserID'];
-	$ContactNumber=$_POST['ContactNumber'];
-	$City=$_POST['City'];
-	
-	$sql="INSERT INTO student(Username,UserID,ContactNumber,City);
-	VALUES('$Username','$UserID','$ContactNumber','$City');";
-	if(mysqli_query($conn,$sql)==True)
-	{
-		echo "Record added successfully";
-	}
-	else
-	{
-		echo "error";
-	}
-}
+	echo "connection error";
 
+}
 
